5614c2473ccce7035ba6f94641ecf6699cd788ef
Nadav Meir <240455053+nadavxmeir@users.noreply.github.com>
2025-11-18 21:22:26 +0200

    c2

--- BoardMessage.java
+++ BoardMessage.java
@@ -1,3 +1,36 @@
-public class BoardMessage {
+import java.util.Date;
+
+enum PriorityType
+{
+    REGULAR,
+    URGENT,
+    CRITICAL
+}
+public class BoardMessage extends Message
+{
+    private PriorityType priority;
+    private Date expirationDate;
     
+    public BoardMessage(String sender, String recipient, String content, Date sendDate, PriorityType priority, Date expirationDate) {
+        super(sender, recipient, content, sendDate);
+        
+        setPriority(priority);
+        setExpirationDate(expirationDate);
+    }
+    public PriorityType getPriority() {
+        return priority;
+    }
+    public void setPriority(PriorityType priority) {
+        this.priority = priority;
+    }
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nPriority: " + priority + "\nExpiration Date: " + expirationDate;
+    }
 }
